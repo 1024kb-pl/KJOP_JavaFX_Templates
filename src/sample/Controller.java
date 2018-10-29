37f7ad8aeced24c8b401872c3998ed694537ec43
1024kb-pl <maniaq@1024kb.pl>
2018-10-29 18:19:20 +0100

    init commit

---
+++ src/sample/Controller.java
@@ -0,0 +1,71 @@
+package sample;
+
+public class Controller {
+
+
+    public void clickButtonAdd() {
+
+    }
+
+    public void clickButtonMinus() {
+
+    }
+
+    public void clickButtonSeven() {
+
+    }
+
+    public void clickButtonEight() {
+
+    }
+
+    public void clickButtonFour() {
+
+    }
+
+    public void clickButtonFive() {
+
+    }
+
+    public void clickButtonOne() {
+
+    }
+
+    public void clickButtonTwo() {
+
+    }
+
+    public void clickButtonMultiply() {
+
+    }
+
+    public void clickButtonDive() {
+
+    }
+
+    public void clickButtonNine() {
+
+    }
+
+    public void clickButtonClear() {
+
+    }
+
+    public void clickButtonSix() {
+
+    }
+
+    public void clickButtonDot() {
+
+    }
+
+    public void clickButtonThree() {
+
+    }
+
+    public void clickButtonZero() {
+
+    }
+
+
+}
